a1c701d2e4e16b6ba0272d96822b3c8716f47991
Hamish Henare <hamishjustin7@gmail.com>
2022-07-02 13:30:21 +1200

    switching pc

--- src/components/NavModule/NavModule.js
+++ src/components/NavModule/NavModule.js
@@ -101,7 +101,7 @@ const NavModule = () => {
                 onClick={toggleSubNav}
                 onKeyDown={toggleSubNav}
               >
-                Products<span>.</span>
+                Projects<span>.</span>
                 <Chevron />
               </button>
 
@@ -116,7 +116,7 @@ const NavModule = () => {
                     onKeyDown={toggleNav}
                     to="/products"
                   >
-                    All Products<span>.</span>
+                    All Projects<span>.</span>
                   </Link>
                 </li>
                 <hr />
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ const Index = () => {
       <Seo title="Home" />
       <Layout>
         <BannerModule
-          title="Welcome to Barcadia"
-          subTitle="Using a combination of Gatsby and Contentful, Barcadia is ready to deploy and simple to use."
+          title="Hamish Henare"
+          subTitle="Full Stack Web Developer Based in Auckland, NZ."
         />
-        <BasicTextModule
+        {/* <BasicTextModule
           title="A super-fast theme that is easy to get started, using the power of
             GatsbyJS"
           content="Using modern CSS properties such as grid, this theme is optmised for
@@ -25,18 +25,18 @@ const Index = () => {
             any device. Future-proofing your product."
           link="/products"
           linkText="View Products"
-        />
-        <PerksModule>
-          <Perk title="The Title" content="The content" />
-        </PerksModule>
+        /> */}
         <Features
-          title="Featured Products from Barcadia."
+          title="Featured projects"
           introduction="Vivamus quam mauris, pulvinar vel mauris id, interdum semper neque. Proin malesuada libero eget tellus scelerisque, id egestas tortor egestas."
         />
-        <LatestPosts
+        {/* <PerksModule>
+          <Perk title="The Title" content="The content" />
+        </PerksModule> */}
+        {/* <LatestPosts
           title="The Latest from Barcadia"
           introduction="Cras scelerisque, tellus sed gravida tincidunt, velit tellus blandit justo, nec viverra augue erat in erat. Maecenas iaculis sed purus non fringilla."
-        />
+        /> */}
       </Layout>
     </>
   )
