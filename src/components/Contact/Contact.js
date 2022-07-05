fc74cf8bca89ebc5389cee3ddc0e63717c3c6352
Hamish Henare <hamishjustin7@gmail.com>
2022-07-05 16:33:38 +1200

    testing html form for netlify

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ import { ContactStyles } from "./ContactStyles"
 const Contact = () => {
   return (
     <ContactStyles className="section">
-      <form name="contact" netlify>
+      <form name="contact" method="POST" data-netlify="true">
         <input placeholder="Your name..." type="text" name="name" />
         <input placeholder="Your email..." type="email" name="email" />
         <textarea
